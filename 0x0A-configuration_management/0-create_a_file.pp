d653f15abdbfbe23b1f5abcf081a0c085ae45b61
Mdlalose4life <blessing1059@gmail.com>
2023-01-11 21:45:43 +0200

    Syntax

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #Create the file /tmp
 file { '/tmp/school':
-  ensure = 'file'
+  ensure = 'file',
   mode = '0744',
   owner = 'www-data',
   group = 'www-data',
-  content = 'I love Puppet'
+  content = 'I love Puppet',
 }
