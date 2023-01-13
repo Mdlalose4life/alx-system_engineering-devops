7b529c946bf14aad6a157a2ccd40e1f0fc8b2caf
Mdlalose4life <blessing1059@gmail.com>
2023-01-13 22:36:00 +0200

    Configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Changes the configuration
+file_line {'password-authenticate':
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
+}
+
+file_line {'private-key':
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school'
+}
