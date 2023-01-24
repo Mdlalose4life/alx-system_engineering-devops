e0c01c92b915337bbd616d1c7f6ef7d62a70fe60
Mdlalose4life <blessing1059@gmail.com>
2023-01-24 14:41:23 +0200

    Indentation debbug

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
-#using puppet to install nginx and set it up
-exec {'/usr/bin/env sudo apt-get update':}
--> package { 'nginx':
+# using puppet to install nginx and set it up
+exec {'/usr/bin/env sudo apt-get update' : }
+-> package {'nginx':
   ensure => installed,
 }
 -> file { '/var/www/html/index.html':
   content => 'Hello World',
 }
--> file_line {
+-> file_line { 'add header'
   ensure => present,
-  path => '/etc/nginx/sites-available/default',
-  line => "\tadd_header X-served-By ${hostname}",
-  after => 'server_name_'
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-served-By ${hostname};",
+  after  => 'server_name_;',
 }
 -> service{'nginx':
   ensure => running,
