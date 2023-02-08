ea13b44f1a0b7f1894c9b21faa7882c987cac6d2
Mdlalose4life <blessing1059@gmail.com>
2023-02-09 00:22:29 +0200

    Changing permissions

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,12 +10,15 @@ if __name__ == '__main__':
     ID = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users" + "/" + ID
 
+    """gettint usernames of the emplyees"""
     response = requests.get(url)
     username = response.json().get('username')
 
+    """getting the todo list of the employees from the API"""
     response = requests.get(url + "/todos")
     todos = response.json()
 
+    """Writing the todo list to a CSV file"""
     with open('{}.csv'.format(ID), 'w') as f:
         for task in todos:
             f.write('"{}","{}","{}","{}"\n'
