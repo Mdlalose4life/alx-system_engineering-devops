9d911977c11dd8ce4946dca9b6a3a702ca50a75c
Mdlalose4life <blessing1059@gmail.com>
2023-05-10 02:08:25 +0200

    Pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    hearders = {
-        "User-Agent": "lenux.0x16-api_advanced:v1.0.0 (by /u/sbusiso_Mdlalose_)"}
+    hearders = {"User-Agent":
+                "lenux.0x16-api_advanced:v1.0.0(by /u/sbusiso_Mdlalose_)"}
     response = requests.get(base_url, headers=hearders, allow_redirects=False)
     if response.status_code == 404:
         return 0
